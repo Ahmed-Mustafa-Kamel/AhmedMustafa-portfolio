1d77436cac9099f1e47d1e9e78b8b428a0fb762a
Ahmed Mustafa Kamel <101ahmedmustafa@gmail.com>
2024-12-07 23:29:58 +0200

    `Update import path for CV file from Ahmed Mustafa.pdf to Ahmed Mustafa Kamel.pdf`

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@
  * @license Apache-2.0
  */
 // import { TypeAnimation } from "react-type-animation";
-import CV from "../../public/Ahmed Mustafa.pdf";
+import CV from "../../public/Ahmed Mustafa Kamel.pdf";
 
 import { ButtonPrimary, ButtonOutLine } from "./Button";
 
