7f38cbce63a7971b97b1450dc2e792f68b7e482d
Ahmed Mustafa Kamel <101ahmedmustafa@gmail.com>
2025-02-02 18:08:49 +0200

    Add background blur effect to Layout component
    - Implement subtle background blur with two positioned, semi-transparent elements
    - Use Tailwind CSS classes to create blurred sky-blue circular backgrounds
    - Position background elements at top-left and bottom-right quadrants of the screen

--- src/components/common/Layout/Layout.jsx
+++ src/components/common/Layout/Layout.jsx
@@ -21,6 +21,11 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <div className="fixed inset-0 -z-10">
+        <div className="absolute top-0 left-1/4 w-96 h-96 bg-sky-400/20 rounded-full blur-[128px]" />
+        <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-sky-400/30 rounded-full blur-[128px]" />
+      </div>
+      
       <Header />
       <main>{children}</main>
       <Footer />
