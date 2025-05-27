8fb246a26805b1ddcc4353bcb385fa32e76cdfe8
Ahmed Mustafa Kamel <ahmed.kamel.dev@gmail.com>
2025-05-27 16:59:08 +0300

    `Update Header component: adjust div class and logo container`

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -9,10 +9,10 @@ const Header = () => {
   const [navOpen, setNavOpen] = useState(false);
   return (
     <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
-      <div className=" max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
+      <div className=" max-w-screen-2xl w-full mx-auto px-6 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
         {/* logo */}
-        <h1>
-          <a href="" className="logo">
+        <h1 className="flex justify-end">
+          <a href="" className="logo ">
             <img
               src="/AMlogo2.png"
               width={80}
